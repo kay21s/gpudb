f017cda222cb36dcb5a4ca1f48e8ce9b5d7f3ad7
Kai Zhang <kay21s@gmail.com>
2013-11-11 20:40:05 -0500

    remove gpudb-explore

--- gpudb-explore/src/msq.h
+++
@@ -1,9 +0,0 @@
-#ifndef _GMM_MSQ_H_
-#define _GMM_MSQ_H_
-
-int msq_init();
-void msq_fini();
-int msq_send_req_evict(int client, long size_needed, int block);
-int msq_send_rep_ack(int client, int ack);
-
-#endif
